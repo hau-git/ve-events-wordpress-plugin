e50e62750ce6fd1876f675892883dae08544ac11
hau-git <github@marcprobst.com>
2025-12-27 20:39:31 +0100

    v1.4.3: Improve field dropdown and Elementor post ID detection

--- includes/elementor/class-dynamic-tag-base.php
+++ includes/elementor/class-dynamic-tag-base.php
@@ -1,29 +1,43 @@
 <?php
 if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+        exit;
 }
 
 abstract class VEV_Elementor_Dynamic_Tag_Base extends \Elementor\Core\DynamicTags\Tag {
 
-	public function get_group(): string {
-		return 've-events';
-	}
+        public function get_group(): string {
+                return 've-events';
+        }
 
-	protected function get_post_id(): int {
-		$post_id = get_the_ID();
+        protected function get_post_id(): int {
+                global $post;
 
-		if ( \Elementor\Plugin::$instance->editor->is_edit_mode() ) {
-			$document = \Elementor\Plugin::$instance->documents->get_current();
-			if ( $document ) {
-				$post_id = $document->get_main_id();
-			}
-		}
+                if ( isset( $post->ID ) && get_post_type( $post->ID ) === VEV_Events::POST_TYPE ) {
+                        return (int) $post->ID;
+                }
 
-		return (int) $post_id;
-	}
+                $queried_id = get_queried_object_id();
+                if ( $queried_id && get_post_type( $queried_id ) === VEV_Events::POST_TYPE ) {
+                        return (int) $queried_id;
+                }
 
-	protected function is_event_post(): bool {
-		$post_id = $this->get_post_id();
-		return $post_id && get_post_type( $post_id ) === VEV_Events::POST_TYPE;
-	}
+                $post_id = get_the_ID();
+                if ( $post_id && get_post_type( $post_id ) === VEV_Events::POST_TYPE ) {
+                        return (int) $post_id;
+                }
+
+                if ( \Elementor\Plugin::$instance->editor->is_edit_mode() ) {
+                        $preview_id = (int) ( $_GET['preview_id'] ?? 0 );
+                        if ( $preview_id && get_post_type( $preview_id ) === VEV_Events::POST_TYPE ) {
+                                return $preview_id;
+                        }
+                }
+
+                return (int) ( $post_id ?: 0 );
+        }
+
+        protected function is_event_post(): bool {
+                $post_id = $this->get_post_id();
+                return $post_id && get_post_type( $post_id ) === VEV_Events::POST_TYPE;
+        }
 }
