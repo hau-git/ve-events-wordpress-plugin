cd7405560117c9ee95ad694f42e54c4b3ddd11e2
hau-git <github@marcprobst.com>
2025-12-27 20:39:29 +0100

    v1.4.3: Improve field dropdown and Elementor post ID detection

--- ve-events.php
+++ ve-events.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: VE Events
  * Description: Adds a lightweight Events post type with WordPress-native admin UI, Schema.org Event markup, and first-class support for Elementor/JetEngine listings.
- * Version: 1.4.2
+ * Version: 1.4.3
  * Requires at least: 6.2
  * Requires PHP: 8.0
  * Author: Marc Probst
@@ -21,7 +21,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 final class VEV_Events {
 
-        public const VERSION = '1.4.2';
+        public const VERSION = '1.4.3';
 
         public const TEXTDOMAIN = 've-events';
         public const POST_TYPE  = 've_event';
